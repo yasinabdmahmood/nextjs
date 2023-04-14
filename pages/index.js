9795eb77fb01ccc23af82fa9a05c609058dd02b6
yasinabdmahmood <abd181103@gmail.com>
2023-04-15 02:36:10 +0300

    Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function Home(){
-    return <h1>Home page</h1>
+    return (
+    <>
+        <h1>Home page</h1>
+        <Link href='/blog'>
+            Blog
+        </Link>
+        <Link href='/product'>
+            Products
+        </Link>
+    </>
+    )
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,21 @@
-function Product() {
+import Link from "next/link";
+
+function Product({productId = 100}) {
     return (
         <div>
-           <h2>Product 1</h2> 
-           <h2>Product 2</h2>
-           <h2>Product 3</h2>
+            <Link href='/'>Home</Link>
+           <h2>
+            <Link href='/product/1'>Product 1</Link>
+           </h2> 
+           <h2>
+            <Link href='/product/2'>Product 2</Link>
+           </h2>
+           <h2>
+            <Link href='/product/3'replace >Product 3</Link>
+           </h2>
+           <h2>
+            <Link href={`/product/${productId}`}>Product {productId}</Link>
+           </h2>
         </div>
     );
 }
